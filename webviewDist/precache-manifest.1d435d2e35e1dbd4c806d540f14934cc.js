7a05e2c910a0192b8dddc0cb46b146579c326f1c
Your Name <you@example.com>
2021-01-20 18:40:04 +0800

    first commit

---
+++ webviewDist/precache-manifest.1d435d2e35e1dbd4c806d540f14934cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57c5ae04c5980957153c88e9e68456f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5642f6e85ece6853462e",
+    "url": "/static/css/main.30fb4986.chunk.css"
+  },
+  {
+    "revision": "6c08eadd553fbcf8e592",
+    "url": "/static/js/2.4e4b8b04.chunk.js"
+  },
+  {
+    "revision": "5642f6e85ece6853462e",
+    "url": "/static/js/main.34948ea1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
